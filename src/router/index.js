f8fe15d82162a744ca2593b072ad011be0c7b6ae
史捷科 <877511684@qq.com>
2020-12-11 20:23:09 +0800

    add createComponents

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,31 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-
+// 引入movie等其他路由
+import Movie from './movie'
+import Mine from './mine'
+import Cinema from './cinema'
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+//   {
+//     path: '/',
+//     name: 'Home',
+//     component: Home
+//   },
+//   {
+//     path: '/about',
+//     name: 'About',
+//     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+//   }
+    //使用movie等其他路由
+    Movie,
+    Mine,
+    Cinema,
+    {
+        // 重定向：当路由不匹配时，把路由指向movie
+        path: '/*',
+        redirect:'/movie'
+    }
 ]
 
 const router = new VueRouter({
